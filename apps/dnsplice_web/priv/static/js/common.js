ede9dcca9eadda6230b460daff2f5c5bd60eba55
Sebastian Green-Husted <ricecake@tfm.nu>
2016-12-31 17:17:18 -0500

    start adding static content

---
+++ apps/dnsplice_web/priv/static/js/common.js
@@ -0,0 +1,11 @@
+requirejs.config({
+	baseUrl: 'static/js',
+	paths: {
+		jquery: 'lib/ext/jquery-2.1.1.min',
+		underscore: 'lib/ext/underscore-min',
+		text: 'lib/ext/text',
+		tpl: 'lib/ext/tpl',
+		template: '../template',
+		KnotConn: 'lib/ext/connection'
+	}
+});
