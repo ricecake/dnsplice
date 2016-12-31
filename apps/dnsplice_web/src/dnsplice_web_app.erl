13fad6220b54f668531d68cca3714a4cfe664a72
Sebastian Green-Husted <ricecake@tfm.nu>
2016-12-31 17:28:58 -0500

    flesh out some api startup stuff

--- apps/dnsplice_web/src/dnsplice_web_app.erl
+++ apps/dnsplice_web/src/dnsplice_web_app.erl
@@ -1,5 +1,10 @@
 %%%-------------------------------------------------------------------
-%% @doc dnsplice_web public API
+%% @doc dnsplice_web OTP Application callback module.
+%%
+%% Provides the behaviour callbacks required for this
+%% app to be started and managed by the OTP application
+%% management framework.
+%%
 %% @end
 %%%-------------------------------------------------------------------
 
@@ -15,12 +20,39 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    dnsplice_web_sup:start_link().
+	case dnsplice_web_sup:start_link() of
+		{ok, Super} ->
+			{ok, _} = cowboy_start(),
+			{ok, Super}
+	end.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
-    ok.
+	ok.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+cowboy_start() ->
+	{ok, Options} = determine_options(),
+	Dispatch      = cowboy_router:compile([
+		{'_', [
+			{"/",             dnsplice_web_page,        index},
+			{"/dashboard",    dnsplice_web_page,        dashboard},
+			{"/ws/",          dnsplice_web_msg_handler, #{}},
+			{"/static/[...]", cowboy_static,          {priv_dir, dnsplice_web, "static/"}}
+		]}
+	]),
+	cowboy:start_clear(dnsplice_web, 25, Options, #{ env => #{ dispatch => Dispatch }}).
+
+determine_options() ->
+	WithIp = case application:get_env(dnsplice_web, ip) of
+		undefined -> [];
+		{ok, IpString} when is_list(IpString) ->
+			{ok, Ip} = inet_parse:address(IpString),
+			[{ip, Ip}];
+		{ok, IpTuple} when is_tuple(IpTuple) -> [{ip, IpTuple}]
+	end,
+	Port = application:get_env(dnsplice_web, port, 8080),
+	{ok, [{port, Port} |WithIp]}.
---
+++ apps/dnsplice_web/src/dnsplice_web_page.erl
@@ -0,0 +1,14 @@
+-module(dnsplice_web_page).
+
+-export([init/2]).
+
+init(Req, Page) ->
+	PageBinary = atom_to_binary(Page, utf8),
+	Template = binary_to_existing_atom(
+		<< "dnsplice_", PageBinary/binary, "_dtl" >>, utf8
+	),
+	{ok, Body} = Template:render([{page, PageBinary} |cowboy_req:bindings(Req)]),
+	Req2 = cowboy_req:reply(200, #{
+		<<"content-type">> => <<"text/html">>
+	}, Body, Req),
+	{ok, Req2, Page}.
