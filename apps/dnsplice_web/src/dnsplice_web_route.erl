54718aa0d95eb573b3e87b11a0456b1932979f3f
Sebastian Green-Husted <ricecake@tfm.nu>
2017-01-04 22:50:36 -0500

    Update dnsplice_web_route.erl

--- apps/dnsplice_web/src/dnsplice_web_route.erl
+++ apps/dnsplice_web/src/dnsplice_web_route.erl
@@ -3,6 +3,9 @@
 -export([init/2]).
 -export([content_types_provided/2]).
 -export([is_authorized/2]).
+-export([allowed_methods/2]).
+-export([content_types_accepted/2]).
+-export([resource_exists/2]).
 -export([list_route_json/2]).
 
 
@@ -13,6 +16,16 @@ content_types_provided(Req, State) ->
 	{[
 		{<<"application/json">>, list_route_json}			
 ], Req, State}.
+
+allowed_methods(Req, State) ->
+	{[<<"GET">>, <<"POST">>], Req, State}
+
+content_types_accepted(Req, State) ->
+	{[{{<<"application">>, <<"json">>, []}, create_route}],
+		Req, State}.
+
+resource_exists(Req, _State) ->
+	{true, Req, index}.
 
 is_authorized(Req, State) ->
 	case cowboy_req:parse_header(<<"authorization">>, Req) of
